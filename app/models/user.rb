7b979ed6c7e20f9db6ccd43f7ce4eeb7ad684528
Munsa1 <munsamibenge65@gmail.com>
2022-03-24 23:43:04 +0200

    Setup devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   # has_many :recipes, foreign_key: 'user_id'
   # has_many :inventories, foreign_key: 'user_id'
   # has_many :foods, foreign_key: 'user_id'
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable
 
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
