ea48f0a3b339eb0bfb5d75b9baaab5d0a6933af2
Timo <timowester@gmail.com>
2022-03-25 00:26:30 +0100

    Correct email setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # has_many :recipes, foreign_key: 'user_id'
   # has_many :inventories, foreign_key: 'user_id'
   # has_many :foods, foreign_key: 'user_id'
-  attr_accessor :email, :encrypted_password
+  # attr_accessor :email, :encrypted_password
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -12,5 +12,5 @@ class User < ApplicationRecord
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
 
-  validates :Name, presence: true, length: { minimum: 3 }
+  # validates :Name, presence: true, length: { minimum: 3 }
 end
