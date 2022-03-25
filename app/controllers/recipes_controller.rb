9beea766cd25b9e1bcb24cf0564e17c48bc3f550
Timo <timowester@gmail.com>
2022-03-25 01:18:11 +0100

    Show and destroy recepis functionality

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,20 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    flash[:notice] = 'The recipe was successfully destroyed.'
+    redirect_to recipes_url
+  end
+end
