26958c46ac3e73dd696812727a04284fe9d5d62f
Timo <timowester@gmail.com>
2022-03-25 02:00:03 +0100

    Add recepi functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,25 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
   def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_food = Recipe.where(recipe_id: params[:recipe_id])
   end
 
   def new
+    @recipe = Recipe.new
+    @foods = Food.all
   end
 
   def create
+    @recipe = Recipe.new(Name:params[:name],preperationtime:params[:preperationtime],cookingtime:params[:cookingtime],description:params[:description],public:params[:public],user_id:current_user.id)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      flash[:notice] = 'Recipe successfully created'
+      redirect_to @recipe
+    else
+      flash[:notice] = 'Something went wrong'
+      render 'new'
+    end
   end
 
   def destroy
